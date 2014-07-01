81eff2d1aadd7c6bdfff30218947caf540ad4e81
PCEgg <ckhk0621@gmail.com>
2014-07-01 18:34:55 +0800

    chang many thing for layouts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # add this to your host name
+  config.action_mailer.default_url_options = { host: 'omr-printeresting.herokuapp.com' }
 end
